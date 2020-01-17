8c0c4add124291dd2ff997da4b9d834301d9d0ba
mmicek02 <micekmichael@gmail.com>
2020-01-16 18:07:24 -0600

    note and folder adjusted for feedback and thinkful support

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -87,7 +87,10 @@ class AddFolder extends Component {
               {this.state.folderName.touched && <ValidationError message={nameError} />}
           </div>
           <div className='buttons'>
-            <button type='submit' onClick={e => this.handleSubmit(e)}>
+            <button 
+              type='submit' 
+              onClick={e => this.handleSubmit(e)}
+            >
               Add folder
             </button>
           </div>
@@ -98,26 +101,3 @@ class AddFolder extends Component {
 }
 
 export default AddFolder;
-
-AddFolder.propTypes = {
-  folderName: (props, propName, componentName) => {
-    // get the value of the prop
-    const prop = props.propName;
-
-    // do the isRequired check
-    if(!prop) {
-      return new Error(`${propName} is required in ${componentName}. Validation Failed`);
-    }
-
-    // check the type
-    if (typeof prop != 'string') {
-      return new Error(`Invalid prop, ${propName} is expected to be a string in ${componentName}. ${typeof prop} found.`);
-    }
-
-    // do the custom check here
-    // using a simple regex
-    if (prop.length < 3 ) {
-      return new Error(`Invalid prop, ${propName} must be min length 5 and begin http(s)://. Validation Failed.`);
-    }
-  },
-}
